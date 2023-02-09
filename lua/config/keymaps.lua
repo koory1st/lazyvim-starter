6d89691bca7662ce15c2ff6faddf79dcf3cdfc91
koory1st <32436334@qq.com>
2023-02-09 23:26:34 +0800

    feat: use whichkey

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,24 +27,43 @@ local function stop_debug()
   require("dap").terminate()
 end
 
+local wk = require("which-key")
+
+wk.register({
+  d = {
+    name = "Debug",
+    c = { "<cmd>DapContinue<cr>", "Start/Continue" },
+    o = { "<cmd>DapStepOver<cr>", "Step Over" },
+    i = { "<cmd>DapStepIn<cr>", "Step In" },
+    b = { "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+    T = { stop_debug, "Stop" },
+  },
+  t = {
+    name = "Terminal",
+    f = { "<cmd>ToggleTerm direction=float<cr>", "ToggleTerm float" },
+    h = { "<cmd>ToggleTerm direction=horizontal<cr>", "ToggleTerm horizontal" },
+    v = { "<cmd>ToggleTerm direction=vertical<cr>", "ToggleTerm vertical" },
+  },
+  T = {
+    name = "Test",
+    n = { "<cmd>lua require('neotest').run.run()<cr>", "Nearest" },
+    f = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "File" },
+    d = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Debug Nearest" },
+    S = { "<cmd>lua require('neotest').run.stop()<cr>", "Stop Nearest" },
+    a = { "<cmd>lua require('neotest').run.attach()<cr>", "Attach Nearest" },
+  },
+}, {
+  prefix = "<leader>",
+})
 n("<F5>", "<cmd>DapContinue<cr>", { desc = "Debugger: Start" })
-n("<leader>dc", "<cmd>DapContinue<cr>", { desc = "Debugger: Start" })
 n("<F17>", "<cmd>DapTerminate<cr>", { desc = "Debugger: Stop" })
-n("<leader>dT", stop_debug, { desc = "Debugger: Stop" })
 n(
   "<F9>",
   "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>",
   { desc = "Debugger: Toggle Breakpoint" }
 )
-n(
-  "<leader>db",
-  "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>",
-  { desc = "Debugger: Toggle Breakpoint" }
-)
 n("<F10>", "<cmd>DapStepOver<cr>", { desc = "Debugger: Step Over" })
-n("<leader>do", "<cmd>DapStepOver<cr>", { desc = "Debugger: Step Over" })
 n("<F11>", "<cmd>DapStepIn<cr>", { desc = "Debugger: Step In" })
-n("<leader>di", "<cmd>DapStepIn<cr>", { desc = "Debugger: Step In" })
 
 n("<leader>cR", function()
   require("telescope.builtin").lsp_references()
@@ -62,7 +81,4 @@ n("<leader>df", function()
   require("telescope").extensions.dap.frames({})
 end, { desc = "List Frames" })
 
-n("<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "ToggleTerm float" })
-n("<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "ToggleTerm horizontal split" })
-n("<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", { desc = "ToggleTerm vertical split" })
 n("<F7>", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
